962404e429d622d9961923795f21206771662db2
Gustavo Hurtado Cristia <ghccristia@gmail.com>
2023-06-28 15:42:35 -0400

    Update Project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,35 @@ import Image from 'next/image'
 
 export default function Home() {
     return (
-        <main>
-            <h1>Home</h1>
+        <main className='flex justify-center items-center container min-h-screen'>
+            {/*QR Component*/}
+            <section>
+                <div className='max-w-sm bg-white rounded-2xl'>
+                    <div className='p-4'>
+                        <Image
+                            src='/images/image-qr-code.png'
+                            className='w-full h-full object-cover object-center drop-shadow-md rounded-xl'
+                            alt='QR code'
+                            width='500'
+                            height='500'
+                            sizes='100vw'
+                        />
+                    </div>
+                    <div className='pt-5 pb-9 px-7'>
+                        <h1 className='pb-4 font-bold text-2xl text-dark_blue'>
+                            Improve your front-end <br />
+                            skills by building projects
+                        </h1>
+                        <p className='text-[1.05rem] text-grayish_blue'>
+                            Scan the QR code to visit Frontend
+                            <br />
+                            Mentor and take your coding skills to
+                            <br />
+                            the next level
+                        </p>
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        colors: {
+            white: 'hsl(0, 0%, 100%)',
+            light_gray: 'hsl(212, 45%, 89%)',
+            grayish_blue: 'hsl(220, 15%, 55%)',
+            dark_blue: 'hsl(218, 44%, 22%)',
+        },
+        extend: {},
+        container: {
+            center: true,
+            padding: '1rem',
+            screens: {
+                sm: '375px',
+                xl: '1440px',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
